2305bf70ec59301fa13a22869f9164372975e912
jazthechicken <jazheelcohleen@gmail.com>
2016-11-18 20:33:45 +0000

    started Arithmetic_tutor class, implementing intialize method

---
+++ arithmetic_tutor.rb
@@ -0,0 +1,14 @@
+require_relative 'player.rb'
+require_relative 'question.rb'
+require_relative 'questionlist.rb'
+
+class Arithmetic_Tutor
+  def initialize
+    @question = QuestionList.new
+  end
+
+  def start_tutor
+
+  end
+
+end
