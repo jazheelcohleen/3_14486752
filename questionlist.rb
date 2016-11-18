a71be15c11e4a581380eeb9d5f19be3137bd32df
jazthechicken <jazheelcohleen@gmail.com>
2016-11-18 20:56:32 +0000

    changed print_list method in questionlist class

--- questionlist.rb
+++ questionlist.rb
@@ -1,24 +1,20 @@
+require_relative 'question.rb'
+
 class QuestionList
 
   def initialize
     @list_of_questions = []
-
+    @result_array = []
   end
 
-  def add_to_list
-    temp = generate_question
-
+  def add_to_list(question)
+    @list_of_questions << question
   end
 
-  def print_list(question)
-
-  end
-
-  def generate_question
-    num1 = rand(1..9)
-    num2 = rand(1..9)
-
-    Question.new(num1, num2)
+  def print_list
+    @list_of_questions.size.times do |i|
+      @list_of_questions[i].print_question
+    end
   end
 
 end
