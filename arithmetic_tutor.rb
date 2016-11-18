f080a22fa696255f942eaba0c05635cc5b84ba7a
jazthechicken <jazheelcohleen@gmail.com>
2016-11-18 22:18:14 +0000

    modified list_questions and question_results

--- arithmetic_tutor.rb
+++ arithmetic_tutor.rb
@@ -10,16 +10,15 @@ class Arithmetic_Tutor
     @questions = QuestionList.new
   end
 
-  def list_questions(no_questions, random_one, random_two)
-    no_questions.times do
-      @questions.add_to_list(Question.new(random_one, random_two))
-    end
+  def list_questions(random_one, random_two)
+    @questions.add_to_list(Question.new(random_one, random_two))
   end
 
-  def question_results
+  def question_results(player_answers)
+    result = "\nYour results were as follows: \n"
     @questions.list_of_questions.size.times do |i|
-      result += @questions.list_of_questions[i].question_result
-      result += "\n"
+      result += "#{@questions.list_of_questions[i].question_result(player_answers[i])}"
     end
+    result
   end
 end
