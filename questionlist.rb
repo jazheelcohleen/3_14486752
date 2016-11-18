1baaca82fc7bce764b27d7a5f8797d705ecf7fc0
jazthechicken <jazheelcohleen@gmail.com>
2016-11-18 19:22:28 +0000

    added random generator

--- questionlist.rb
+++ questionlist.rb
@@ -5,12 +5,20 @@ class QuestionList
 
   end
 
-  def add_to_list(question)
+  def add_to_list
+    temp = generate_question
 
   end
 
   def print_list(question)
 
+  end
+
+  def generate_question
+    num1 = rand(1..9)
+    num2 = rand(1..9)
+
+    Question.new(num1, num2)
   end
 
 end
