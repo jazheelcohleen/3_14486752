e62f7ea1d862919752280a8a3f2a5b719ba3e3ee
jazthechicken <jazheelcohleen@gmail.com>
2016-11-18 21:46:52 +0000

    added question_results method in arithmetic_tutor class

--- arithmetic_tutor.rb
+++ arithmetic_tutor.rb
@@ -3,19 +3,23 @@ require_relative 'question.rb'
 require_relative 'questionlist.rb'
 
 class Arithmetic_Tutor
+
+  attr_reader :questions
+
   def initialize
     @questions = QuestionList.new
   end
 
-  def list_questions(no_questions, seed, random_one, random_two)
-    srand(seed)
+  def list_questions(no_questions, random_one, random_two)
     no_questions.times do
       @questions.add_to_list(Question.new(random_one, random_two))
     end
   end
 
-  def print_questions
-
+  def question_results
+    @questions.list_of_questions.size.times do |i|
+      result += @questions.list_of_questions[i].question_result
+      result += "\n"
+    end
   end
-
 end
