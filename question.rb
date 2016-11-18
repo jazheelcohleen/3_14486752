2ea6b6b79e1f36da5dc5ca81627bfb73e466622c
jazthechicken <jazheelcohleen@gmail.com>
2016-11-18 19:14:09 +0000

    initialised question

---
+++ question.rb
@@ -0,0 +1,24 @@
+class Question
+
+  def initialize(num1, num2)
+    @num1 = num1
+    @num2 = num2
+    @result = num1 + num2
+  end
+
+  def print_question
+    "What is #{@num1} + #{@num2}?"
+
+  end
+
+  def question_result
+    if
+
+    end
+  else
+
+
+  end
+
+
+end
